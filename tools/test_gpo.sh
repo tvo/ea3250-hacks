158098e95eec0f763555eac52c0f25e635b3dc9e
Tobi Vollebregt <tobivollebregt@gmail.com>
2012-08-06 00:32:47 +0200

    tool: test_gpo: Repeatedly toggle the value of an output pin

---
+++ tools/test_gpo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Author: Tobi Vollebregt
+
+if [ x"$1" = x"" ] || [ x"$2" = x"" ]; then
+	echo "Usage: $0 <pin id> <pin name>"
+	exit 0
+fi
+
+cd /sys/class/gpio
+echo $1 > export
+trap "echo $1 > unexport; exit 0" 2
+
+[ -e $2/direction ] && echo out > $2/direction
+
+value=0
+while true; do
+	value=$((1-value))
+	echo -n "value : $value" $'\r'
+	echo $value > $2/value
+	sleep 3
+done
