b09f9d7eebff5ab7a985aca0bb4e9f26a9407cac
Tobi Vollebregt <tobivollebregt@gmail.com>
2012-08-05 23:55:29 +0200

    module: lpc32xx_adc: Remove debugging code

--- modules/lpc32xx_adc.c
+++ modules/lpc32xx_adc.c
@@ -22,9 +22,6 @@
 
 #define MOD_NAME "lpc32xx-adc"
 
-static const char *to_string(u32 value);
-static void dump_regs(void);
-
 /* Core */
 
 static int adc(int source) {
@@ -81,9 +78,6 @@ static struct platform_device *lpc32xx_adc_device;
 static int __init lpc32xx_adc_probe(struct platform_device *pdev) {
 	int i, err;
 
-	printk(KERN_INFO "lpc32xx_adc_probe\n");
-	dump_regs(); /* pre-init register state */
-
 	/* Check if touch screen is enabled, bail out if it is */
 	if ((__raw_readl(LPC32XX_ADC_CTRL) & _BIT(0)) != 0)
 		return -EBUSY;
@@ -114,14 +108,10 @@ static int __init lpc32xx_adc_probe(struct platform_device *pdev) {
 	}
 
 	lpc32xx_adc_device = pdev;
-
-	dump_regs(); /* post-init register state */
 	return 0;
 }
 
 static int __devexit lpc32xx_adc_remove(struct platform_device *pdev) {
-	printk(KERN_INFO "lpc32xx_adc_remove\n");
-
 	/* Power down ADC */
 	__raw_writel(0, LPC32XX_ADC_CTRL);
 
@@ -129,8 +119,6 @@ static int __devexit lpc32xx_adc_remove(struct platform_device *pdev) {
 	__raw_writel(0, LPC32XX_CLKPWR_ADC_CLK_CTRL);
 
 	lpc32xx_adc_device = NULL;
-
-	dump_regs(); /* post-remove register state */
 	return 0;
 }
 
@@ -147,8 +135,6 @@ static struct platform_driver lpc32xx_adc_driver = {
 static int __init lpc32xx_adc_init(void) {
 	struct platform_device *pdev;
 
-	printk(KERN_INFO "lpc32xx_adc_init\n");
-
 	pdev = platform_create_bundle(&lpc32xx_adc_driver, lpc32xx_adc_probe, NULL, 0, NULL, 0);
 	if (IS_ERR(pdev))
 		return PTR_ERR(pdev);
@@ -156,8 +142,6 @@ static int __init lpc32xx_adc_init(void) {
 }
 
 static void __exit lpc32xx_adc_exit(void) {
-	printk(KERN_INFO "lpc32xx_adc_exit\n");
-
 	platform_device_unregister(lpc32xx_adc_device);
 	platform_driver_unregister(&lpc32xx_adc_driver);
 }
@@ -168,25 +152,3 @@ module_exit(lpc32xx_adc_exit);
 MODULE_AUTHOR("Tobi Vollebregt <tobivollebregt@gmail.com>");
 MODULE_DESCRIPTION("LPC32XX ADC Driver");
 MODULE_LICENSE("GPL");
-
-/* Utilities */
-
-static const char *to_string(u32 value) {
-	static char buf[64];
-	u32 i, j;
-
-	for (i = j = 0; i < 32; ++i) {
-		buf[j++] = (value & (1 << (31 - i))) ? '1' : '0';
-		if ((i & 7) == 7) buf[j++] = ' ';
-	}
-	buf[--j] = 0;
-	return buf;
-}
-
-static void dump_regs(void) {
-	printk(KERN_INFO "ADC_SELECT     = %s\n", to_string(__raw_readl(LPC32XX_ADC_SELECT)));
-	printk(KERN_INFO "ADC_CTRL       = %s\n", to_string(__raw_readl(LPC32XX_ADC_CTRL)));
-	printk(KERN_INFO "ADC_VALUE      = %s\n", to_string(__raw_readl(LPC32XX_ADC_VALUE)));
-	printk(KERN_INFO "ADC_CLK_CTRL   = %s\n", to_string(__raw_readl(LPC32XX_CLKPWR_ADC_CLK_CTRL)));
-	printk(KERN_INFO "ADC_CLK_CTRL_1 = %s\n", to_string(__raw_readl(LPC32XX_CLKPWR_ADC_CLK_CTRL_1)));
-}
