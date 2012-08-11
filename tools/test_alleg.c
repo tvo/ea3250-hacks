dc23fddbebfc8ebe007bfa5f11b9fadba271b8d7
Tobi Vollebregt <tobivollebregt@gmail.com>
2012-08-12 00:35:44 +0200

    tool: test_alleg: Fix segfault

--- tools/test_alleg.c
+++ tools/test_alleg.c
@@ -65,15 +65,20 @@ int main() {
 	if (allegro_init() != 0)
 		return 1;
 
-	if (install_timer() != 0)
+	/* Without keyboard installed, Allegro segfaults in
+	   the parent process after the fork in init_console(). */
+	if (install_keyboard() != 0)
 		return 2;
+
+	if (install_timer() != 0)
+		return 3;
 
 	set_color_depth(16);
 
 	if (set_gfx_mode(GFX_FBCON, 240, 320, 0, 0) != 0) {
 		TRACE("set_gfx_mode failed: %s\n", allegro_error);
 		set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
-		return 3;
+		return 4;
 	}
 
 	/* last column (239) is not visible */
@@ -90,3 +95,4 @@ int main() {
 	clear_bitmap(screen);
 	return 0;
 }
+END_OF_MAIN()
