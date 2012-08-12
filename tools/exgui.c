96fe779785212736d16ef1d393b1dd87f95776f6
Tobi Vollebregt <tobivollebregt@gmail.com>
2012-08-12 23:53:00 +0200

    tool: exgui: Set proper graphics mode
    Half the examples fall off the screen though...

--- tools/exgui.c
+++ tools/exgui.c
@@ -311,8 +311,10 @@ int main(int argc, char *argv[])
    install_mouse();
    install_timer();
 
-   if (set_gfx_mode(GFX_AUTODETECT, 640, 480, 0, 0) != 0) {
-      if (set_gfx_mode(GFX_SAFE, 640, 480, 0, 0) != 0) {
+   set_color_depth(16);
+
+   if (set_gfx_mode(GFX_AUTODETECT, 240, 320, 0, 0) != 0) {
+      if (set_gfx_mode(GFX_SAFE, 240, 320, 0, 0) != 0) {
 	 set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
 	 allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
 	 return 1;
