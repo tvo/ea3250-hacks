b33ebbed64ce9630b840cd9514d5f6fb3d3c5241
Tobi Vollebregt <tobivollebregt@gmail.com>
2012-09-18 21:20:18 +0200

    tool: test_serial: Make configurable

--- tools/test_serial.py
+++ tools/test_serial.py
@@ -3,6 +3,9 @@
 
 import serial, sys
 
-ser = serial.serial_for_url(sys.argv[1])
+n = len(sys.argv)
+device = sys.argv[1] if n > 1 else '/dev/ttyS1'
+baud = int(sys.argv[2]) if n > 2 else 9600
+ser = serial.serial_for_url(device, baud)
 print ser.write('1234567890')
 print ser.read(10)
